c9a262a66adb84ea7e7d84dde460a6c2759779eb
Doug O'Riordan <oriordan@mail.be>
2016-02-27 02:38:06 +0000

    Making SoftToken actually work

--- yubistack/softtoken.py
+++ yubistack/softtoken.py
@@ -15,38 +15,64 @@ from .utils import (
 class SoftToken:
     """
     SoftToken generator class
+
+    Usage:
+    >>> st = SoftToken('tetetetetecc', '8792ebfe26cc', 'ecde18dbe76fbd0c33330f1c354871db')
+    >>> st.counter = 19
+    >>> st.timestamp_low = 49320
+    >>> st.timestamp_high = 0
+    >>> st.session_use = 16
+    >>> for i in range(0, 5):
+    ...     st.otp()
+    ...
+    'tetetetetecccrgkcdjhtfebjvvvufuuccvetbinrhuv'
+    'teteteteteccvtjfbfdrictrndbeubdtccbhhdblinvb'
+    'teteteteteccfjtjkfhrnbcrgjnrcvrktffnirdkhukn'
+    'teteteteteccfjjkiivgrcnvbkgiffckkglentvtekfb'
+    'teteteteteccvhfftjitevuvuehhbvcktcgndnuiblbr'
+    >>>
     """
 
-    def __init__(self, public_id):
+    def __init__(self, public_id, internalname, aeskey):
         self.public_id = public_id
+        self.internalname = internalname
+        self.aeskey = aeskey
         self.counter = 0
         self.timestamp_low = 0
         self.timestamp_high = 0
         self.session_use = 0
 
-    def generate_otp(self):
-        """ Generate OTP token for `public_id` """
+    def otp(self):
+        """ Generate OTP token """
+        _random = '%.4x' % randint(0, 65536)
         counter_hex = '%.4x' % self.counter
         low_hex = '%.4x' % self.timestamp_low
-        high_hex = '%.4x' % self.timestamp_high
-        use_hex = '%x' % self.session_use
-        otp = (self.public_id +
-               counter_hex[2:4] + counter_hex[0:2] +
-               low_hex[2:4] + low_hex[0:2] +
-               high_hex +
-               use_hex)
+        high_hex = '%.2x' % self.timestamp_high
+        use_hex = '%.2x' % self.session_use
         self.session_use += 1
-        return otp
+        token = (self.internalname +
+                 counter_hex[2:4] +
+                 counter_hex[0:2] +
+                 low_hex[2:4] +
+                 low_hex[0:2] +
+                 high_hex +
+                 use_hex +
+                 _random[2:4] +
+                 _random[0:2])
+        crc_hex = '%.4x' % (~calculate_crc(token) & 0xffff)
+        token += crc_hex[2:4] + crc_hex[0:2]
+        return self.public_id + aes128ecb_encrypt(self.aeskey, token)
 
-def otp(aeskey, internalname, counter, low, high, use):
-    """ Generate OTP token """
-    random = '%.4x' % randint(0, 65536)
-    otp = (internalname +
-           counter[2:4] + counter[0:2] +
-           low[2:4] + low[0:2] +
-           high +
-           use + 
-           random[2:4] + random[0:2])
-    crc_hex = '%.4x' % (~calculate_crc(otp) & 0xffff)
-    otp += crc_hex[2:4] + crc_hex[0:2]
-    return aes128ecb_encrypt(aeskey, otp)
+def main():
+    """ Main program """
+    token = SoftToken('tetetetetecc', '8792ebfe26cc', 'ecde18dbe76fbd0c33330f1c354871db')
+    token.counter = 19
+    token.timestamp_low = 49320
+    token.timestamp_high = 0
+    token.session_use = 16
+    for _ in range(0, 5):
+        print(token.otp())
+
+if __name__ == '__main__':
+    main()
+
