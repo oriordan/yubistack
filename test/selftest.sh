7f3edd5ee052a8d4b5463d1a21832e7205346856
Timothy Hilliard <thilliard@linkedin.com>
2024-01-04 02:01:08 -0500

    Fix psql test execution

--- test/selftest.sh
+++ test/selftest.sh
@@ -44,7 +44,8 @@ EOF
 elif [ "x$DB" = "xpgsql" ]; then
   dbuser=yubistack
   dbpass=yubistack_pw
-  psql_cmd="PGPASSWORD=$dbpass psql -U $dbuser"
+  export PGPASSWORD=$dbpass
+  psql_cmd="psql -U $dbuser"
   $psql_cmd -c 'create database ykksm;'
   $psql_cmd ykksm < ykksm-db.sql
   dbrun_ykksm="$psql_cmd ykksm -c"
